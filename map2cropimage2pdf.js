c8ac165a36a0a4fcee5e17ab337bd97435741b4d
CHILLET Christophe <christophe.chillet@vanoise-parcnational.fr>
2016-09-06 11:01:57 +0200

    Update map2cropimage2pdf.js

--- map2cropimage2pdf.js
+++ map2cropimage2pdf.js
@@ -1,8 +1,21 @@
+// Objectif : exporter une partie de la carte (fonds carto et objets vectoriels) dans un fichier pdf
+// Le code ci-dessous vient d'un environnement AngularJS 1.4
+// Les blocs suivants sont à mettre dans les bons fichiers html et js
+
+// <Partie HTML>
+// Récupérer et appeler le plugin Leaflet-image et la librairie PDFMake dans votre fichier html
 <script src="chemin/leaflet-image/leaflet-image.js"></script>
 <script type="text/javascript" src="chemin/pdfmake/pdfmake.js"></script>
 <script type="text/javascript" src="chemin/pdfmake/vfs_fonts.js"></script>
 
-var exportimg = function () {
+// <Partie Javascript>
+// Les fonctions export2pdf() et map2cropimage() se trouvent dans un directive AngularJs
+// La fonction export2pdf() est appelé sur un bouton dans un fichier html depuis une balise ng-click
+// La fonction export2pdf() fait appel à la fonction map2cropimage() en gérant les promise, c'est à dire que l'ouverture du pdf
+// contenant l'image ne se fait pas avant que l'image soit constituée des différents tuiles raster et des objets vectoriels et 
+// que le rognage de l'image soit fait
+
+var map2cropimage = function () {
     var deferred = $q.defer();
     var map = mapService.getMap();
     var sizemap = map.getSize();
@@ -32,7 +45,7 @@ var exportimg = function () {
     return deferred.promise;
 }
 
-$scope.shootmaptest = function () {
+$scope.export2pdf = function () {
     var deferred = $q.defer();
     // var promise = createcanvas.call()
     var promise = exportimg.call();
