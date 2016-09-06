4e6ef08d481c83b2b878d6ff88de6f3f8cc926cf
CHILLET Christophe <christophe.chillet@vanoise-parcnational.fr>
2016-09-06 11:43:32 +0200

    Update map2cropimage2pdf.js

--- map2cropimage2pdf.js
+++ map2cropimage2pdf.js
@@ -1,14 +1,18 @@
 // Objectif : exporter une partie de la carte (fonds carto et objets vectoriels) dans un fichier pdf
-// Le code ci-dessous vient d'un environnement AngularJS 1.4
+// Le code ci-dessous vient d'un environnement AngularJS 1.4 et Leaflet
 // Les blocs suivants sont à mettre dans les bons fichiers html et js
 
-// <Partie HTML>
+// ******************** <Partie HTML> ********************
+// Gestion des objets vectorielles dans la map en canvas pour la transformation en image
+// valable pour Leaflet < v1 (sinon voir doc Leaflet-image)
+<script>L_PREFER_CANVAS = true;</script>
+
 // Récupérer et appeler le plugin Leaflet-image et la librairie PDFMake dans votre fichier html
-<script src="chemin/leaflet-image/leaflet-image.js"></script>
-<script type="text/javascript" src="chemin/pdfmake/pdfmake.js"></script>
+<script src="chemin/leaflet-image/leaflet-image.js"></script> // https://github.com/mapbox/leaflet-image
+<script type="text/javascript" src="chemin/pdfmake/pdfmake.js"></script> // http://pdfmake.org/#/
 <script type="text/javascript" src="chemin/pdfmake/vfs_fonts.js"></script>
 
-// <Partie Javascript>
+// ******************** <Partie Javascript> ********************
 // Les fonctions export2pdf() et map2cropimage() se trouvent dans un directive AngularJs
 // La fonction export2pdf() est appelé sur un bouton dans un fichier html depuis une balise ng-click
 // La fonction export2pdf() fait appel à la fonction map2cropimage() en gérant les promise, c'est à dire que l'ouverture du pdf
@@ -23,43 +27,59 @@ var pdfFileDefinition = {
         {
             image: '',
         },
-      {
-        text: 'Test pour du texte'
-      }
+        {
+            text: 'Fruits and Calories'
+        }
     ],
     styles: {
-      header: {
-        bold: true,
-        color: '#000',
-        fontSize: 11
-      },
-      // Autres propriétés
-      }
+        header: {
+            bold: true,
+            contentolor: '#000',
+            fontSize: 11
+        },
+        // Autres propriétés
     }
-  };
+}
 
+// Fonction qui crée une image depuis les tuiles raster et des objets vectoriels de la map courante rognage de l'image
 var map2cropimage = function () {
     var deferred = $q.defer(); // gestion promise pour assurer constitution complète image
     var map = mapService.getMap(); // Récupération objet map (ici map se trouve dans un service AngularJs mapService)
     var sizemap = map.getSize();
-    leafletImage(map, function(err, canvas) {
+    leafletImage(map, function(err, canvas) { // plugin pour la création de l'image depuis la map
         var itemImage = canvas.toDataURL('image/jpeg');
         var finalCanvas = document.createElement('canvas');
+        
+        // taille du canvas qui permet de créer l'image finale = détermine la taille de l'image finale
         finalCanvas.width = 400;
         finalCanvas.height = 300;
+        
         var context = finalCanvas.getContext('2d');
         var imageObj = new Image();
         imageObj.src = itemImage;
+        
+        // taille du morceau de la map qu'on souhaite récupérer (taille map = 1680x660)
+        // todo : partie à rendre dynamique selon la taille de la map afin d'avoir des proportions plutôt que des constantes
         var sourceWidth = 400;
         var sourceHeight = 300;
+        // coordonnées à partir desquelles le morceau de map est récupéré
+        // pour cet example une image de 400x300 centrée sur la map est récupérée
         var sourceX = sizemap.x / 2 - sourceWidth / 2;
-        var sourceY = sizemap.y / 2 - sourceHeight / 2;;
+        var sourceY = sizemap.y / 2 - sourceHeight / 2;
+        
+        // création de l'image depuis le canvas
         context.drawImage(imageObj, sourceX, sourceY, sourceWidth, sourceHeight, 0, 0, sourceWidth, sourceHeight);
         var imgfin = new Image();
+        
+        // bloc permettant de faire afficher l'image pour tester
+        // créer <div id="images"></div> dans html
         // imgfin.src = canvas2.toDataURL("image/png");
         // document.getElementById('images').innerHTML = '';
         // document.getElementById('images').appendChild(imgfin);
+        
+        // Insertion de l'image dans l'objet qui va constituer le PDF
         pdfFileDefinition.content[0].image = finalCanvas.toDataURL("image/png");
+        
         deferred.resolve();
     });
     return deferred.promise;
